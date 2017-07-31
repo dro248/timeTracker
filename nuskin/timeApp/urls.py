64f82e2ee1a7bd11a78b0335df5ce5b7cb78f380
david <david.ostler001@gmail.com>
2017-07-31 11:43:43 -0600

    initial commit

---
+++ nuskin/timeApp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth.views import login, logout, logout_then_login
+
+urlpatterns = [
+	# url(r'^login/$', views.user_login, name='login'),
+	url(r'^/$', 					views.dashboard, 	name='dashboard'),
+	
+	# login /logout urls
+	url(r'^login/$', 				login, 				name='login'),
+	url(r'^logout/$', 				logout, 			name='logout'),
+	url(r'^logout-then-login/$', 	logout_then_login, 	name='login_then_login'),
+
+]
