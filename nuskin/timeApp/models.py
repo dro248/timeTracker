c79cef0576be067ecd032f3fb9966ce679c5c41e
david <david.ostler001@gmail.com>
2017-08-02 16:14:47 -0600

    Requested days saved correctly in database on server; BUG FOUND (front-end): date-range selector does not always submit the correct dates

--- nuskin/timeApp/models.py
+++ nuskin/timeApp/models.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class RequestDate(models.Model):
+	username = models.CharField(max_length=30)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	start_date = models.DateField()
+	end_date = models.DateField(null=True, blank=True)		
+	approval_status = models.CharField(max_length=10)
+	# {'PENDING', 'APPROVED', 'DENIED'}
+
+	def approve(self):
+		self.approval_status = 'APPROVED'
+
+	def deny(self):
+		self.approval_status = 'DENIED'
+
+	def __str__(self):
+		return str(self.username) + " (start:" + str(self.start_date) + "  -  end:" + str(self.end_date) + ")"
--- nuskin/timeApp/views.py
+++ nuskin/timeApp/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from .models import RequestDate
+import datetime
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
@@ -61,7 +63,17 @@ def request_time_off(request):
 			req_day_off = req_string.split('&')[3].split('=')[1]
 			print 'day off:', req_day_off
 
-			# Create RequestedDate entry to database
+			# Create RequestDate entry to database
+			year = int(req_day_off.split('-')[0])
+			month = int(req_day_off.split('-')[1])
+			day = int(req_day_off.split('-')[2])
+			requestedDate = RequestDate(username=username, 
+				start_date = datetime.date(year, month, day), 
+				end_date=None, 
+				approval_status='PENDING')
+			requestedDate.save()
+			print ("requestedDate:", requestedDate)
+
 
 		if req_type == 'multiple':
 			print "multiple"
@@ -70,11 +82,23 @@ def request_time_off(request):
 			end_date = req_string.split('&')[4].split('=')[1]
 			print "start date:", start_date
 			print "end date:", end_date 
-			
-			# Create RequestedDate entry to database
 
+			# Create RequestedDate entry to database
+			start_year = int(start_date.split('-')[0])
+			start_month = int(start_date.split('-')[1])
+			start_day = int(start_date.split('-')[2])
+			
+			end_year = int(end_date.split('-')[0])
+			end_month = int(end_date.split('-')[1])
+			end_day = int(end_date.split('-')[2])
+
+			requestedDate = RequestDate(username=username, 
+				start_date = datetime.date(start_year, start_month, start_day), 
+				end_date = datetime.date(end_year, end_month, end_day), 
+				approval_status='PENDING')
+			requestedDate.save()
 		else:
 			pass
 
 
-	return HttpResponse('DONE')
+	return HttpResponse('OK')
